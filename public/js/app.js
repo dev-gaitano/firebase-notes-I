650ccd8c99fdc83f5b8269ec25b05694e7ec6873
dev-gaitano <dev.gaitano@gmail.com>
2025-07-02 15:40:25 +0300

    Added animations

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,24 @@
-// Debug: alert("app.js run")
+// Theme Toggle
 const themeButton = document.getElementById('theme-button');
 const body = document.body;
 
 themeButton.addEventListener('click', () => {
   body.classList.toggle('light');
+});
+
+// Animations
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  });
+})
+
+const hiddenElements = document.getElementsByClassName('hidden')
+hiddenElements.forEach((element) => {
+  observer.observe(element)
 });
