6189ab251c12f573df09ae1fde54a594a345495d
dev-gaitano <dev.gaitano@gmail.com>
2025-07-03 11:43:40 +0300

    Added dark theme and light theme icon toggle

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,33 @@
 // Theme Toggle
 const themeButton = document.getElementById('theme-button');
 const body = document.body;
+const lightThemeIcon = document.getElementById('light-theme-icon')
+const darkThemeIcon = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+darkThemeIcon.setAttribute("id", "dark-theme-icon");
+darkThemeIcon.setAttribute("class", "icon-primary");
+darkThemeIcon.setAttribute("viewBox", "0 0 24 24");
+darkThemeIcon.setAttribute("fill", "none");
+
+const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+path.setAttribute("d", "M12 3a9 9 0 1 0 9 9c0-.46-.04-.92-.1-1.36a5.389 5.389 0 0 1-4.4 2.26 5.403 5.403 0 0 1-3.14-9.8c-.44-.06-.9-.1-1.36-.1z");
+path.setAttribute("fill", "none");
+path.setAttribute("stroke", "currentColor");
+path.setAttribute("stroke-width", "2");
+path.setAttribute("stroke-linecap", "round");
+path.setAttribute("stroke-linejoin", "round");
+
+darkThemeIcon.appendChild(path);
 
 themeButton.addEventListener('click', () => {
-  body.classList.toggle('light');
+  const isLight = body.classList.toggle('light');
+
+  if (isLight) {
+    lightThemeIcon.replaceWith(darkThemeIcon);
+  } else {
+    darkThemeIcon.replaceWith(lightThemeIcon);
+  }
 });
+
 
 // Animations
 const observer = new IntersectionObserver((entries) => {
